699bd9653392bc2a67b6175413f7116959e130ba
TinaCheong <kanade11020512@gmail.com>
2020-04-17 15:53:58 +0800

    <Setting Fragment> write a function to save setting data on onPause

--- app/src/main/java/com/tina/voicetubetest/setting/SettingFragment.kt
+++ app/src/main/java/com/tina/voicetubetest/setting/SettingFragment.kt
@@ -9,15 +9,67 @@ import com.tina.voicetubetest.databinding.FragmentSettingBinding
 
 class SettingFragment : Fragment(){
 
+    lateinit var binding: FragmentSettingBinding
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
 
-        val binding = FragmentSettingBinding.inflate(inflater, container, false)
+        binding = FragmentSettingBinding.inflate(inflater, container, false)
 
+        binding.autoPlaySwitch.isChecked = SettingManager.autoPlay
+        binding.subtitleSyncSwitch.isChecked = SettingManager.subtitle
+        binding.playingVideoSwitch.isChecked = SettingManager.videoPlay
+        binding.videoNotificationSwitch.isChecked = SettingManager.videonNotify
+        binding.learnNotificationSwitch.isChecked = SettingManager.learningNotify
 
         return binding.root
+    }
+
+    override fun onPause() {
+        super.onPause()
+        saveSetting(binding.autoPlaySwitch, binding.autoPlaySwitch.isChecked)
+        saveSetting(binding.subtitleSyncSwitch, binding.subtitleSyncSwitch.isChecked)
+        saveSetting(binding.playingVideoSwitch, binding.playingVideoSwitch.isChecked)
+        saveSetting(binding.videoNotificationSwitch, binding.videoNotificationSwitch.isChecked)
+        saveSetting(binding.learnNotificationSwitch, binding.learnNotificationSwitch.isChecked)
+    }
+
+
+    private fun saveSetting(view: View, isOpened: Boolean){
+        when(view){
+            binding.autoPlaySwitch -> {
+                when(isOpened){
+                    true -> SettingManager.autoPlay = true
+                    else -> SettingManager.autoPlay = false
+                }
+            }
+            binding.subtitleSyncSwitch -> {
+                when(isOpened){
+                    true -> SettingManager.subtitle = true
+                    else -> SettingManager.subtitle = false
+                }
+            }
+            binding.playingVideoSwitch -> {
+                when(isOpened){
+                    true -> SettingManager.videoPlay = true
+                    else -> SettingManager.videoPlay = false
+                }
+            }
+            binding.videoNotificationSwitch -> {
+                when(isOpened){
+                    true -> SettingManager.videonNotify = true
+                    else -> SettingManager.videonNotify = false
+                }
+            }
+            binding.learnNotificationSwitch -> {
+                when(isOpened){
+                    true -> SettingManager.learningNotify = true
+                    else -> SettingManager.learningNotify = false
+                }
+            }
+        }
     }
 }
